78ce83c8103f88b6157a47cd2916b747c46e9f97
Sarah-kasande <s.kasande@alustudent.com>
2024-06-12 07:18:09 -0700

    file

---
+++ main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# File to store student records
+STUDENT_FILE="students-list_1023.txt"
+
+#Main menu
+while true; do
+	echo "----------------------------"
+	echo "ALU REGISTRATION SYSTEM"
+	 echo "----------------------------"
+    echo "1. Create Student"
+    echo "2. View Students"
+    echo "3. Delete Student"
+    echo "4. Update Student"
+    echo "5. Exit"
+    read -p "Choose an option: " option
