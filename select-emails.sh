bcd4ee40e4164c1fe0591fd79613a9c9970051a1
Kelvin364 <k.rwihimba@alustudent.com>
2024-06-12 18:13:25 +0200

    doubt clearance and adding more functionality

--- select-emails.sh
+++ select-emails.sh
@@ -1,14 +1,14 @@
-#!/usr/env/bin/bash
+#!/bin/bash
 
 # File icontaining student records
 STUDENT_FILE="students-list_1023.txt"
 
 # Output file for student emails
-EMAIL_FILE="student-emails.sh"
+EMAIL_FILE="student-emails.txt"
 
 if [ -f $STUDENT_FILE ]; then
-    cut -f ',' -f 2 $STUDENT_FILE > $EMAIL_FILE
+    cut -d ',' -f 2 $STUDENT_FILE > $EMAIL_FILE
     echo "Student emails have been saved to $EMAIL_FILE."
 else
     echo "No student records found."
-if
+fi
